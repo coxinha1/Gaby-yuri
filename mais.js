f9d8797e1385228d7f8cb173772e73831d3bcbed
coxinha1 <125052661+coxinha1@users.noreply.github.com>
2023-09-06 11:42:48 -0300

    Add files via upload

--- mais.js
+++ mais.js
@@ -1,3 +1,8 @@
-document.querySelector('.tecla_pom');
+// pom -----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
 
-document.querySelector('#_som_tecla_pom').play();
+const listaDeTeclas =document.querySelectorAll('.tecla'); 
+
+listaDeTeclas[0].onclick = tocaSomPom;
