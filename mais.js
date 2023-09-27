6493a7c7992fdc69164f76c73178c2eda14aa772
coxinha1 <125052661+coxinha1@users.noreply.github.com>
2023-09-27 11:39:23 -0300

    Add files via upload

--- mais.js
+++ mais.js
@@ -1,26 +1,26 @@
 function tocaSom (idElementoAudio) {
-
     document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); 
 
-let contador =0;
-
 //enquanto
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const instrumento = listaDeTeclas[contador].classList[1];
-
-    console.log(instrumento);
-
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom')
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }  
+    
+    tecla.nokeydowm = function () {
+        tecla.classList.add('ativa');
+    }
 
-    contador = contador + 1;
-
-    console.log(contador);
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
-  
